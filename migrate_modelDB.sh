5243bc2df84347e199f7c6dba1009a09cb42b215
GonxFH <gonzalo.fernandez@murciaeduca.es>
2024-01-19 22:22:10 +0100

    Error corregido script migracion modelos 3

--- migrate_modelDB.sh
+++ migrate_modelDB.sh
@@ -1,4 +1,11 @@
+# build_files.sh
+echo "Instalando requerimientos..."
 pip install -r requirements.txt
-python3 manage.py makemigrations
-python3 manage.py migrate
 
+# Automatic migrations
+echo "Iniciando migraciones..."
+python3 manage.py makemigrations && python3 manage.py migrate
+
+# Colecta de staticsFiles en theme/static/
+echo "Colectando StaticFiles..."
+python3 manage.py collectstatic --noinput
